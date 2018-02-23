f7f413c220a20d8f1e04b8618c2162a87d550be1
Claudia Reyna <pacha94@protonmail.com>
2018-02-23 13:26:50 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,25 +1,13 @@
-var index = require("../index.js")
-
-describe('what-is-a-test', () => {
-  describe('Name', () => {
-    it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan')
-    })
-  })
-
-
-  describe('Height', () => {
-    it('is less than 40', () => {
-      expect(index.height).toBeLessThan(40)
-    })
-  })
-
-  describe('Message', () => {
-    it('gives the name and height', () => {
-      expect(index.message).toInclude(index.name)
-      expect(index.message).toInclude(index.height)
-    })
-  })
-
-})
-
+// declare the variable
+var word
+ 
+// assign a value to the variable
+word = 'bird'
+ 
+console.log(word) // 'bird'
+ 
+// assign another value to the variable
+word = 'dog'
+ 
+console.log(word) // 'dog'
+window.word
